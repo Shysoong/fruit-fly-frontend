7c2af77fc62c0acedef77b0c91d730dd6ae0aaf0
Tony <shysoong@gmail.com>
2023-02-06 10:55:13 +0800

    - add test scene

---
+++ src/views/test.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <a-scene background="color: #ECECEC">
+      <a-assets>
+        <a-asset-item id="flyModel" src="https://cdn.aframe.io/test-models/models/glTF-2.0/virtualcity/VC.gltf"></a-asset-item>
+      </a-assets>
+      <a-entity gltf-model="#flyModel"></a-entity>
+    </a-scene>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'test',
+  methods: {
+    test() {
+
+    }
+  }
+}
+</script>
+<style scoped>
+
+</style>
